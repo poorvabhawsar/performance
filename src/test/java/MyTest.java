682a5fc6385466f7c4926b4e05622568c5d7b4f9
poorva.bhawsar <poorva.bhawsar@servicenow.com>
2025-07-03 03:12:33 +0530

    initial commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,15 @@
+import com.microsoft.demo.Demo;
+import org.junit.Test;
+
+public class MyTest {
+    @Test
+    public void test_method_1() {
+        Demo d = new Demo();
+        d.DoSomething(true);
+        // main 4
+    }
+
+    @Test
+    public void test_method_2() {
+    }
+}
